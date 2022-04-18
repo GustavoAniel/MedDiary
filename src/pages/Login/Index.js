ba88bbdf00b82316e242651aa51015c7a469a4fb
Gustavo <aniel.amtimil@yahoo.com.br>
2022-04-18 10:14:53 -0300

    Tela de Login estilizado e Importado Bibliotecas

--- src/pages/Login/Index.js
+++ src/pages/Login/Index.js
@@ -1,25 +1,40 @@
 import React, { useState } from 'react';
-import { NativeBaseProvider, Box, ScrollView, Stack, Input, FormControl, Divider, Center, Button } from "native-base";
-import { StyleSheet } from 'react-native';
+import { NativeBaseProvider, Box, ScrollView, Stack, Input, FormControl, Divider, Center, Button, Image } from "native-base";
+import { StyleSheet, Text } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+
+const config = {
+    dependencies: {
+        "linear-gradient" : LinearGradient
+    }
+}
 
 export default function Login(){
     const [login, setLogin] = useState();
     const [senha, setSenha] = useState();
 
-    function logar(){
+    function logar(login, senha){
+        if(login == '' || senha == ''){
+            console.log('Está nulo amigão!')
+        }else{
 
+        }
     }
 
     return(
-        <NativeBaseProvider>
-            <Box style={[style.background]}> 
-                <Box style={[style.boxForm]}>
+        <NativeBaseProvider config={config}>
+            <Box style={[style.background]} bg={{ linearGradient: {colors: ['#15D36D', '#15A5D3'], start: [1, 0], end: [0, 1]}}}> 
+                <Box style={[style.boxForm]} p={5} rounded={5} >
+                    <Center>
+                        <Image w={40} h={40}  source={require('../../../assets/logo-meddiary.png')} />
+                    </Center>
+
                     <FormControl isRequired>
                         <Stack mx="4" safeArea space={10}>
 
                             <Box>
                                 <FormControl.Label _android={{_text:{color: 'white', fontSize: 20}}}>Usuário</FormControl.Label>
-                                <Input variant='underlined' type="login" />
+                                <Input color='white' variant='underlined' type="login" onChangeText={(login) => setLogin(login)} />
                                 <FormControl.ErrorMessage >
                                     Atleast 6 characters are required.
                                 </FormControl.ErrorMessage>
@@ -27,14 +42,14 @@ export default function Login(){
                             
                             <Box>
                                 <FormControl.Label _android={{_text:{color: 'white', fontSize: 20}}}>Senha</FormControl.Label>
-                                <Input variant='underlined' type="password"  />
+                                <Input color='white' variant='underlined' type="password" onChangeText={(senha) => setSenha(senha)} />
                                 <FormControl.ErrorMessage >
                                     Atleast 6 characters are required.
                                 </FormControl.ErrorMessage>
                             </Box>
                             
                             <Box alignItems="center">
-                                <Button size={'lg'} onPress={() => console.log("hello world")}>Logar</Button>
+                                <Button backgroundColor={'#15D36D'} size={'lg'} onPress={() => logar(login, senha)}>Logar</Button>
                             </Box>
                         </Stack>
                     </FormControl>
@@ -53,7 +68,7 @@ const style = StyleSheet.create({
     },
     boxForm: {
         width: '100%',
-        maxWidth: 300,
+        maxWidth: 400,
         alignSelf: 'center'
     }
 })
