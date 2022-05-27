addab6195a2a22ed718befb24396c4574a9562dc
Gustavo <aniel.amtimil@yahoo.com.br>
2022-05-26 23:33:47 -0300

    Alteração de arvores de diretório

--- App.js
+++ App.js
@@ -6,26 +6,29 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import  Listar  from './src/pages/Listar_Todos/Index';
 import Menu from './src/pages/Menu/Index';
-import Login from './src/pages/Login/Index';
+import Login from './src/pages/Usuario/Login/Index';
 import Listar_Lembretes from './src/pages/Lembretes/Listagem/Index';
 import Cadastrar_Lembretes from './src/pages/Lembretes/Cadastro/Index';
-import Listar_Diario from './src/pages/Diario/Listagem/Index'
+import Listar_Diario from './src/pages/Diario/Listagem/Index';
+import {SSRProvider} from '@react-aria/ssr';
+
 
 const Stack = createStackNavigator();
 
 
 export default function App(){
   return(
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName='Login' screenOptions={{headerShown: false}}>
-        <Stack.Screen name='Login' component={Login} />
-        <Stack.Screen name='Listar' component={Listar} />
-        <Stack.Screen name='Menu' component={Menu} />
-        <Stack.Screen name="Listar_Lembretes" component={Listar_Lembretes} />
-        <Stack.Screen name='Cadastrar_Lembretes' component={Cadastrar_Lembretes}/>
-        <Stack.Screen name='Listar_Diario' component={Listar_Diario}/>
-      </Stack.Navigator>
-    </NavigationContainer>
-    
+    <SSRProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName='Login' screenOptions={{headerShown: false}}>
+          <Stack.Screen name='Login' component={Login} />
+          <Stack.Screen name='Listar' component={Listar} />
+          <Stack.Screen name='Menu' component={Menu} />
+          <Stack.Screen name="Listar_Lembretes" component={Listar_Lembretes} />
+          <Stack.Screen name='Cadastrar_Lembretes' component={Cadastrar_Lembretes} />
+          <Stack.Screen name='Listar_Diario' component={Listar_Diario} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SSRProvider>
   )
 }
