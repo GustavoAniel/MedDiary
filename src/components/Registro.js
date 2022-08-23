4d1c755e5cdf357e6ff3bbfc7871aa998d878aa5
Gustavo <aniel.amtimil@yahoo.com.br>
2022-08-22 21:58:33 -0300

    Adicionado data do registro com ternário

--- src/components/Registro.js
+++ src/components/Registro.js
@@ -9,6 +9,7 @@ import { Text } from "react-native";
 import Icon from "react-native-vector-icons/Entypo";
 import { style, styleRegistro } from "../styles/Style";
 import IconIonicons from "react-native-vector-icons/Ionicons";
+import { horaRegistro } from "../lib/horaRegistro";
 import { dataRegistro } from "../lib/dataRegistro";
 
 export default function Registro({
@@ -17,36 +18,45 @@ export default function Registro({
     titulo,
     descricao,
     data_criacao,
+    day_repeat
 }) {
     return (
-        <Box key={id} style={[styleRegistro.card]}>
-            <HStack flex={1}>
-                <VStack>
-                    <Box style={[styleRegistro.miniCard]} bg={gradient}>
-                        <Icon
-                            name="emoji-happy"
-                            size={30}
-                            style={[styleRegistro.iconeTag]}
-                        />
-                    </Box>
-                </VStack>
-                <VStack flex={1} paddingLeft={2}>
-                    <Text style={[styleRegistro.textTitulo]}>{titulo}</Text>
-                    <ScrollView>
-                        <Text style={[styleRegistro.textDesc]}>
-                            {descricao}
-                        </Text>
-                    </ScrollView>
-                    <Box style={[styleRegistro.iconGeral]}>
-                        <IconIonicons
-                            name="time-outline"
-                            size={20}
-                            color={"#ACACAC"}
-                        />
-                        <Text style={[styleRegistro.textIcons]}>{dataRegistro(data_criacao)[1]}</Text>
-                    </Box>
-                </VStack>
-            </HStack>
+        <Box key={id}>
+            {day_repeat ?  <Text style={styleRegistro.textData}>
+                {dataRegistro(data_criacao)}
+            </Text>: null}
+           
+            <Box style={[styleRegistro.card]}>
+                <HStack flex={1}>
+                    <VStack>
+                        <Box style={[styleRegistro.miniCard]} bg={gradient}>
+                            <Icon
+                                name="emoji-happy"
+                                size={30}
+                                style={[styleRegistro.iconeTag]}
+                            />
+                        </Box>
+                    </VStack>
+                    <VStack flex={1} paddingLeft={2}>
+                        <Text style={[styleRegistro.textTitulo]}>{titulo}</Text>
+                        <ScrollView>
+                            <Text style={[styleRegistro.textDesc]}>
+                                {descricao}
+                            </Text>
+                        </ScrollView>
+                        <Box style={[styleRegistro.iconGeral]}>
+                            <IconIonicons
+                                name="time-outline"
+                                size={20}
+                                color={"#ACACAC"}
+                            />
+                            <Text style={[styleRegistro.textIcons]}>
+                                {horaRegistro(data_criacao)}
+                            </Text>
+                        </Box>
+                    </VStack>
+                </HStack>
+            </Box>
         </Box>
     );
 }
